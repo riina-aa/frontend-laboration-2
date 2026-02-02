cb4fb894b75a7502ec265895d47a96589a3e5be2
Riina <riina.e.aaltonen@gmail.com>
2026-02-02 13:36:16 +0100

    Skrivit kod för att få fram data via fetch

---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    init();
+}); //Ladda sidan innan kod körs
+
+//funktion för att starta igång koden
+function init() {
+    getData();
+}
+
+//funktion för att hämta data
+async function getData() {
+    const url = "https://webbutveckling.miun.se/files/ramschema.json";
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        displayData(data);
+
+    } catch (error) {
+        console.error("Felmeddelande: " + error)
+    }
+}
+
+function displayData(kurser) {
+    const tableDataEl = document.querySelector("#tbody");
+
+    kurser.forEach(kurs => {
+        tableDataEl.innerHTML += `
+            <tr>
+                <td>${kurs.code}</td>
+                <td>${kurs.coursename}</td>
+                <td>${kurs.progression}</td>
+            </tr > 
+        `;
+    });
+};
